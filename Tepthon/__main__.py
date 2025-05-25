61ff29867d2bbd46128694876e7b2de10e674b40
Abu .. <qmwsy2641@gmail.com>
2025-05-25 15:59:27 +0300

    Add files via upload

--- Tepthon/__main__.py
+++ Tepthon/__main__.py
@@ -1,20 +1,29 @@
 from . import *
-
-import contextlib
-import os
 import sys
 import time
-from .core.helper import time_formatter
+from Tepthon.core.helper import time_formatter
 from Tepthon.load_plug import load
 from telethon.errors import SessionRevokedError
-from .utils import (
-    join_dev,
-    main_process,
+from .utils import join_dev, main_process
+from Tepthon.config import Var
+from telethon.sessions import StringSession
+from Tepthon.core.client import TepthonClient
+
+start_time = time.time()
+
+# إنشاء الكلاينت
+jmubot = TepthonClient(
+    session=StringSession(Var.STRING_SESSION),
+    api_id=Var.API_ID,
+    api_hash=Var.API_HASH
 )
 
-jmubot.me.phone = None
+# تسجيل الدخول
+jmubot.run_in_loop(jmubot.start_client(bot_token=Var.BOT_TOKEN))
 
+# الآن يمكن التعامل مع .me
 if not jmubot.me.bot:
+    setattr(jmubot.me, "phone", None)
     jmdB.set_key("OWNER_ID", jmubot.me.id)
     jmdB.set_key("NAME", jmubot.full_name)
 
@@ -30,12 +39,10 @@ except Exception as meo:
 
 jmubot.loop.create_task(join_dev())
 
-
 async def load_plugins():
     load(["plugins/basic", "plugins/assistant", "plugins/account", "plugins/fun", "plugins/group"])
 
-
-jmubot.loop.run_until_complete(load_plugins())
+jmubot.run_in_loop(load_plugins())
 
 LOGS.info(f"⏳ تم استغراق {time_formatter((time.time() - start_time) * 1000)} ميللي ثانية لبدء تشغيل سورس تيبثون.")
 
@@ -53,4 +60,4 @@ try:
     LOGS.info(f"تم بنجاح تشغيل البوت المساعد من @Tepthon")
 except SessionRevokedError:
     LOGS.info(f"جلسة البوت المساعد [@{asst.me.username}] فشلت لكن سيتم تشغيل سورس الحساب فقط")
-    jmubot.run() 
+    jmubot.run()
