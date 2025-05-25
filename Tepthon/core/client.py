9bba4029dc150dd8804a7c4da856d8661ef28e94
Abu .. <qmwsy2641@gmail.com>
2025-05-25 15:14:29 +0300

    Update client.py

--- Tepthon/core/client.py
+++ Tepthon/core/client.py
@@ -13,6 +13,7 @@ from telethon.errors import (
     ApiIdInvalidError,
     AuthKeyDuplicatedError,
 )
+from telethon.sessions import StringSession
 from telethon.tl.types import User
 
 from Tepthon.config import Var
@@ -21,11 +22,21 @@ from Tepthon.core.helper import fast_download
 from Tepthon.core.logger import LOGS, TelethonLogger
 from resources import blacklisted_users
 
+
 class TepthonClient(TelegramClient):
     me: User
 
-    def __init__(self, session, api_id: int = Var.API_ID, api_hash: str = Var.API_HASH,
-                 bot_token=None, logger: Logger = LOGS, log_attempt=True, exit_on_error=True, *args, **kwargs):
+    def __init__(
+        self,
+        session,
+        api_id: int = Var.API_ID,
+        api_hash: str = Var.API_HASH,
+        logger: Logger = LOGS,
+        log_attempt=True,
+        exit_on_error=True,
+        *args,
+        **kwargs,
+    ):
         self._cache = {}
         self._dialogs = []
         self._handle_error = exit_on_error
@@ -33,23 +44,23 @@ class TepthonClient(TelegramClient):
         self.logger = logger
         self._thumb = {}
         kwargs["base_logger"] = TelethonLogger
-        super().__init__(session, api_id=api_id, api_hash=api_hash, **kwargs)
-        self.run_in_loop(self.start_client(bot_token=bot_token))
-        self.dc_id = self.session.dc_id
+        super().__init__(
+            StringSession(session),
+            api_id=api_id,
+            api_hash=api_hash,
+            connection_retries=10,
+            retry_delay=2,
+            auto_reconnect=True,
+            *args,
+            **kwargs,
+        )
+        self.parse_mode = "html"
 
-    def __repr__(self):
-        return f"<Tepthon Client :\n self: {self.full_name}\n bot: {self._bot}\n>"
-
-    @property
-    def __dict__(self):
-        if self.me:
-            return self.me.to_dict()
-
-    async def start_client(self, **kwargs):
+    async def start_client(self, bot_token=None):
         if self._log_at:
             self.logger.info("❃ جاري تسجيل الدخول...")
         try:
-            await self.start(**kwargs)
+            await self.start(bot_token=bot_token)
         except ApiIdInvalidError:
             self.logger.critical("◙ الأيبي أيدي والأيبي هاش غير متطابقين")
             sys.exit()
@@ -69,10 +80,30 @@ class TepthonClient(TelegramClient):
             setattr(self.me, "phone", None)
             me = self.full_name
         if self.uid in blacklisted_users:
-            self.log.error(f"({me} - {self.uid}) ~ لا يمكنك استخدام سورس تيبثون أنت محظور بسبب مخالفتك سياسة الاستخدام @Tepthon")
+            self.logger.error(f"({me} - {self.uid}) ~ لا يمكنك استخدام سورس تيبثون أنت محظور بسبب مخالفتك سياسة الاستخدام @Tepthon")
             sys.exit(1)
         if self._log_at:
             self.logger.info(f"❃ تم تسجيل الدخول كـ {me}")
         self._bot = await self.is_bot()
 
-    
+    def run_in_loop(self, function):
+        return self.loop.run_until_complete(function)
+
+    def run(self):
+        self.run_until_disconnected()
+
+    def add_handler(self, func, *args, **kwargs):
+        if func in list(map(lambda e: e[0], self.list_event_handlers())):
+            return
+        self.add_event_handler(func, *args, **kwargs)
+
+    @property
+    def full_name(self):
+        return utils.get_display_name(self.me)
+
+    @property
+    def uid(self):
+        return self.me.id
+
+    def to_dict(self):
+        return dict(inspect.getmembers(self))
