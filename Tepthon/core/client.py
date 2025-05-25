0c33a6a3be9c5dccd2f5460bf91bd5d4954fea91
Abu .. <qmwsy2641@gmail.com>
2025-05-25 15:50:29 +0300

    Update client.py

--- Tepthon/core/client.py
+++ Tepthon/core/client.py
@@ -1,63 +1,112 @@
-from . import *
+import contextlib
+import inspect
+import os
 import sys
 import time
-from Tepthon.core.helper import time_formatter
-from Tepthon.load_plug import load
-from telethon.errors import SessionRevokedError
-from .utils import join_dev, main_process
-from Tepthon.config import Var
+from datetime import datetime
+from logging import Logger
+
+from telethon import hints, utils, TelegramClient
+from telethon.errors import (
+    AccessTokenExpiredError,
+    AccessTokenInvalidError,
+    ApiIdInvalidError,
+    AuthKeyDuplicatedError,
+)
 from telethon.sessions import StringSession
-from Tepthon.core.client import TepthonClient
+from telethon.tl.types import User
 
-start_time = time.time()
+from Tepthon.config import Var
+from database import jmdB
+from Tepthon.core.helper import fast_download
+from Tepthon.core.logger import LOGS, TelethonLogger
+from resources import blacklisted_users
 
-# إنشاء الكلاينت
-jmubot = TepthonClient(
-    session=StringSession(Var.SESSION),
-    api_id=Var.API_ID,
-    api_hash=Var.API_HASH
-)
 
-# تسجيل الدخول
-jmubot.run_in_loop(jmubot.start_client(bot_token=Var.BOT_TOKEN))
+class TepthonClient(TelegramClient):
+    me: User
 
-# الآن يمكن التعامل مع .me
-if not jmubot.me.bot:
-    setattr(jmubot.me, "phone", None)
-    jmdB.set_key("OWNER_ID", jmubot.me.id)
-    jmdB.set_key("NAME", jmubot.full_name)
+    def __init__(
+        self,
+        session,
+        api_id: int = Var.API_ID,
+        api_hash: str = Var.API_HASH,
+        logger: Logger = LOGS,
+        log_attempt=True,
+        exit_on_error=True,
+        *args,
+        **kwargs,
+    ):
+        self._cache = {}
+        self._dialogs = []
+        self._handle_error = exit_on_error
+        self._log_at = log_attempt
+        self.logger = logger
+        self._thumb = {}
+        kwargs["base_logger"] = TelethonLogger
 
-LOGS.info("جاري تثبيت تيبثون...")
+        super().__init__(
+            StringSession(session),
+            api_id=api_id,
+            api_hash=api_hash,
+            connection_retries=10,
+            retry_delay=2,
+            auto_reconnect=True,
+            *args,
+            **kwargs,
+        )
+        self.parse_mode = "html"
 
-try:
-    LOGS.info("- يتم إعـداد الإعدادات .......")
-    jmubot.loop.run_until_complete(main_process())
-    LOGS.info("تم إعداد إعدادات تيبثون ✅")
-except Exception as meo:
-    LOGS.error(f"- {meo}")
-    sys.exit()
+    async def start_client(self, bot_token=None):
+        if self._log_at:
+            self.logger.info("❃ جاري تسجيل الدخول...")
+        try:
+            await self.start(bot_token=bot_token)
+        except ApiIdInvalidError:
+            self.logger.critical("◙ الأيبي أيدي والأيبي هاش غير متطابقين")
+            sys.exit()
+        except (AuthKeyDuplicatedError, EOFError):
+            if self._handle_error:
+                self.logger.critical("⎆ كود السيشن أو التوكن منتهي اصنع كود جديد وأضفه إلى المتغيرات...")
+                return sys.exit()
+            self.logger.critical("❃ سيشن الحساب/توكن الحساب منتهي الصلاحية.")
+        except (AccessTokenExpiredError, AccessTokenInvalidError):
+            jmdB.del_key("BOT_TOKEN")
+            self.logger.critical("❃ توكن البوت منتهي أو غير صالح، اصنع بوت جديد من @Botfather وأضفه مع المتغير BOT_TOKEN")
+            sys.exit()
+        self.me = await self.get_me()
+        if self.me.bot:
+            me = f"@{self.me.username}"
+        else:
+            setattr(self.me, "phone", None)
+            me = self.full_name
+        if self.uid in blacklisted_users:
+            self.logger.error(
+                f"({me} - {self.uid}) ~ لا يمكنك استخدام سورس تيبثون أنت محظور بسبب مخالفتك سياسة الاستخدام @Tepthon"
+            )
+            sys.exit(1)
+        if self._log_at:
+            self.logger.info(f"❃ تم تسجيل الدخول كـ {me}")
+        self._bot = await self.is_bot()
 
-jmubot.loop.create_task(join_dev())
+    def run_in_loop(self, function):
+        return self.loop.run_until_complete(function)
 
-async def load_plugins():
-    load(["plugins/basic", "plugins/assistant", "plugins/account", "plugins/fun", "plugins/group"])
+    def run(self):
+        self.run_until_disconnected()
 
-jmubot.run_in_loop(load_plugins())
+    def add_handler(self, func, *args, **kwargs):
+        if func in list(map(lambda e: e[0], self.list_event_handlers())):
+            return
+        self.add_event_handler(func, *args, **kwargs)
 
-LOGS.info(f"⏳ تم استغراق {time_formatter((time.time() - start_time) * 1000)} ميللي ثانية لبدء تشغيل سورس تيبثون.")
+    @property
+    def full_name(self):
+        return utils.get_display_name(self.me)
 
-LOGS.info(
-    """
-    ╔══════════════════════════════════════════╗
-    ║       ✅ تم تنصيب وتشغيل سورس تيبثون بنجاح             ║ 
-    ║       تابع آخر التحديثات من خلال قناة @Tepthon            ║
-    ╚══════════════════════════════════════════╝
-    """
-)
+    @property
+    def uid(self):
+        return self.me.id
 
-try:
-    asst.run()
-    LOGS.info(f"تم بنجاح تشغيل البوت المساعد من @Tepthon")
-except SessionRevokedError:
-    LOGS.info(f"جلسة البوت المساعد [@{asst.me.username}] فشلت لكن سيتم تشغيل سورس الحساب فقط")
-    jmubot.run()
+    def to_dict(self):
+        return dict(inspect.getmembers(self))
